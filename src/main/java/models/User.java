14e7a7bf5b523293f2e10d05d5de294c8ea897c5
Alexander Orlovsky <javaprofi@gmx.de>
2019-04-19 13:58:03 +0200

    test for checking first and lastname of user

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -2,5 +2,39 @@ package models;
 
 public class User
 {
+	private String userFirstName;
+    private String userLastName;
+    
+	public User()
+	{	
+	  this.setUserFirstName("undefined");
+	}
+	
+	public User(String firstName)
+	{	
+		this.userFirstName = firstName;
+	}
+	
+	public String getUserFirstName()
+	{
+		return userFirstName;
+	}
+
+	public void setUserFirstName(String userFirstName)
+	{
+		this.userFirstName = userFirstName;
+	}
+
+	public String getUserLastName()
+	{
+		return userLastName;
+	}
+
+	public void setUserLastName(String userLastName)
+	{
+		this.userLastName = userLastName;
+	}
+
+
 
 }
--- src/test/java/ModelTests/UserModelTest.java
+++ src/test/java/ModelTests/UserModelTest.java
@@ -22,4 +22,29 @@ class UserModelTest
 		assertNotNull(userObj,"User Object was not created!");
 	}
 
+	@Test
+	void checkIfuserNameSetUp() {
+		String firstNameOfUser = "John";
+		userObj.setUserFirstName(firstNameOfUser);
+		
+		assertEquals(firstNameOfUser,userObj.getUserFirstName(), "Firstname of user was not correctly set up!");
+	}
+	
+	@Test
+	void checkIfuserLastnameSetUp() {
+		String lastNameOfUser = "Doe";
+		userObj.setUserLastName(lastNameOfUser);
+		
+		assertEquals(lastNameOfUser,userObj.getUserLastName(), "Lastname of user was not correctly set up!");
+	}
+	
+	@Test
+	void checkIfUserFirstAndLastNameSetUp() {
+		String firstNameOfUser = "John";
+		String lastNameOfUser = "Doe";
+		userObj.setUserFirstName(firstNameOfUser);
+		userObj.setUserLastName(lastNameOfUser);
+		assertEquals(firstNameOfUser+" "+lastNameOfUser,userObj.getUserFirstName()+" "+userObj.getUserLastName(), "Firstname and Lastname of user was not correctly set up!");
+	}
+	
 }

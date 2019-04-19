0a3547618bf4e2bef5be277b6a0a643eabdff212
Alexander Orlovsky <javaprofi@gmx.de>
2019-04-19 10:17:22 +0200

    restructing the project

---
+++ src/main/java/main/Application.java
@@ -0,0 +1,17 @@
+/******************************************
+ Created by: RXSUBRAM a.k.a angoothachap
+ Created on: 4/15/2019 11:19 AM
+ ******************************************/
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}

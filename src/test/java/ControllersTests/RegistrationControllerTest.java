007b9c1944809770b6db65009e58d4684bead0d6
Alexander Orlovsky <javaprofi@gmx.de>
2019-04-22 12:25:06 +0200

    Add tests for static content, updated properties and junit4 dependency

--- src/test/java/ControllersTests/RegistrationControllerTest.java
+++ src/test/java/ControllersTests/RegistrationControllerTest.java
@@ -1,18 +1,10 @@
 package ControllersTests;
-
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-
-import org.apache.catalina.filters.SetCharacterEncodingFilter;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import controllers.RegistrationController;
@@ -29,13 +21,13 @@ class RegistrationControllerTest
     	   mockMvc = MockMvcBuilders.standaloneSetup(regController).build();
     
     }
-	
+	   
     @Test
     public void checkThatRegistrationPageHasAValidViewName() throws Exception {
-    	   String expectedResult="New member registration";
     	   mockMvc.perform(get("/regnewuser"))
     	        
     	   .andExpect(status().isOk())
     	   .andExpect(view().name("regnewuserform"));
     }
+    
 }
---
+++ src/test/java/ControllersTests/StaticContentTest.java
@@ -0,0 +1,41 @@
+package ControllersTests;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import main.Application;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = Application.class)
+@AutoConfigureMockMvc
+public class StaticContentTest
+{
+	@Autowired
+	private MockMvc mockMvc;
+	
+    @Test
+    public void checkThatStaticContentCSSCanBeAccessed() throws Exception {
+    	   mockMvc.perform(get("/css/test.css"))     
+    	   .andExpect(status().isOk());  	   
+    }
+    
+    @Test
+    public void checkThatStaticContentIMGCanBeAccessed() throws Exception {
+    	   mockMvc.perform(get("/img/test.jpg"))     
+    	   .andExpect(status().isOk());  	   
+    }
+    
+    @Test
+    public void checkThatStaticContentJSCanBeAccessed() throws Exception {
+    	   mockMvc.perform(get("/js/test.js"))     
+    	   .andExpect(status().isOk());  	   
+    }
+}

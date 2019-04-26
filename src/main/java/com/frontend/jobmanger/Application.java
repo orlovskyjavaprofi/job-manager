d1238486466e30201b7f87a4b7bd1e62392c1bf6
Alexander Orlovsky <javaprofi@gmx.de>
2019-04-26 15:44:19 +0200

    some clean up, but proper handler for adding new user must be written

--- src/main/java/com/frontend/jobmanger/Application.java
+++ src/main/java/com/frontend/jobmanger/Application.java
@@ -1,7 +1,3 @@
-/******************************************
- Created by: RXSUBRAM a.k.a angoothachap
- Created on: 4/15/2019 11:19 AM
- ******************************************/
 package com.frontend.jobmanger;
 
 import org.springframework.boot.SpringApplication;

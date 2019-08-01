7c87b2c6efb22199c98295f3c79daf9c62975aa1
Alexander Orlovsky <javaprofi@gmx.de>
2019-08-01 19:14:16 +0200

    add view list of job applications

--- src/main/java/com/frontend/jobmanger/controller/ShowApplicationsListController.java
+++ src/main/java/com/frontend/jobmanger/controller/ShowApplicationsListController.java
@@ -1,17 +1,85 @@
 package com.frontend.jobmanger.controller;
 
+import java.util.SortedSet;
+
+import javax.annotation.Resource;
+import javax.validation.Valid;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import com.frontend.jobmanager.service.InMemoryUserService;
+
+import models.User;
+import models.UserApplication;
+import models.UserEmploymentState;
+import models.UserSexState;
 
 @Controller
 public class ShowApplicationsListController
 {
+	@Resource
+	private InMemoryUserService inMemUserService;
+	private User actualUserWhichIslogedIn;
+	
 	@GetMapping(value = { "/memberarea/userAccountOffice/showListOfUserJobApplications" })
 	@ResponseStatus(code = HttpStatus.FORBIDDEN)
 	public String notAuthAccessToMemberArea()
 	{
 		return "restrictedAccess";
 	}
+	
+	@GetMapping(value = { "/memberarea/userAccountOffice/showListOfUserJobApplications/" })
+	public String authUserAccessShowJobApplicationList(@RequestParam String uName,
+			@Valid @ModelAttribute User currentLogedUser, BindingResult bindingResult, Model userModel) {
+		String pathToUserApplShowListPage = "restrictedAccess";
+		SortedSet<UserApplication> userJobApplications;
+		
+		caseForRunningUnitTestsWhenInMemServiceNotAvailable();
+		actualUserWhichIslogedIn = inMemUserService.findUserByNickname(uName);
+		
+		if(actualUserWhichIslogedIn != null) {
+			currentLogedUser = actualUserWhichIslogedIn;
+			userModel.addAttribute("userLoginName",uName);
+			userJobApplications=actualUserWhichIslogedIn.getUserApplicationsSet();			
+			userModel.addAttribute("userJobApplications",userJobApplications);
+			pathToUserApplShowListPage = "memberarea/listOfUserJobApplications";
+		}
+		
+		return pathToUserApplShowListPage;
+	}
+	
+	private void caseForRunningUnitTestsWhenInMemServiceNotAvailable()
+	{
+		if (inMemUserService == null)
+		{
+			inMemUserService = new InMemoryUserService();
+			createTestUserAndSaveItInRepoForTestPurposeOnly();
+		}
+	}
+	
+	private void createTestUserAndSaveItInRepoForTestPurposeOnly()
+	{
+		User testUser = new User();
+		testUser.setUserFirstName("Thomas");
+		testUser.setUserLastName("Jefferson");
+		testUser.setUserBirthDate("01.12.1978");
+		testUser.setUserEmail("cooldude@io.com");
+		testUser.setUserCity("Detroit");
+		testUser.setUserStreetName("BerlinerStreet");
+		testUser.setUserStreetNumber(123);
+		testUser.setUserCountryName("USA");
+		testUser.setUserNickName("testdude000");
+		testUser.setCurrentUserSexState(UserSexState.MALE);
+		testUser.setCurrentEmploymentState(UserEmploymentState.SELFEMPLOYED);
+		String pass = "tuxtux123456";
+		inMemUserService.saveNewUserWithRandomPass(testUser, pass);
+	}
+	
 }

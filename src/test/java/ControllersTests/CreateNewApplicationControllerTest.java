eaeeb64998d6069771e7647b68432526213c3b8c
Alexander Orlovsky <javaprofi@gmx.de>
2019-08-07 08:36:57 +0200

    another small cleanup

--- src/test/java/ControllersTests/CreateNewApplicationControllerTest.java
+++ src/test/java/ControllersTests/CreateNewApplicationControllerTest.java
@@ -1,12 +1,12 @@
 package ControllersTests;
 
+import models.CompanySalutationType;
+import models.CompanyType;
+
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import models.CompanySalutationType;
-import models.CompanyType;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
